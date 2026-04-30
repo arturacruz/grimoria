acabbe7b16ccf08f73b416ce0d991b6949752e0d
Artur Cruz <arturacruz@icloud.com>
2026-04-29 22:58:44 -0300

    Card drag component

---
+++ Assets/Combat/Creature/Card/DragComponent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class DragComponent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+{
+    private bool isMouseOver = false;
+    private bool selected = false;
+
+    private void Update()
+    {
+        if (!selected)
+            return;
+
+        var mousePos = Camera.main.ScreenToWorldPoint(
+            Mouse.current.position.ReadValue()
+        );
+        
+        // Unity fucking converts the position to -10 since that is the camera Z.
+        mousePos.z = 0;
+
+        transform.position = mousePos;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isMouseOver = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (isMouseOver && !selected)
+            selected = true;
+        else 
+            selected = false;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isMouseOver = false;
+    }
+}

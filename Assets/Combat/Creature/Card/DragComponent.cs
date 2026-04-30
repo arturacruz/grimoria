10405bbdd80cdcaddada7de03bcc2826669e3dab
Artur Cruz <arturacruz@icloud.com>
2026-04-29 23:08:22 -0300

    Element scriptable objects

--- Assets/Combat/Creature/Card/DragComponent.cs
+++ Assets/Combat/Creature/Card/DragComponent.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using UnityEditor.UI;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
---
+++ Assets/Combat/Creature/Element/ElementObject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ElementObject", menuName = "Scriptable Objects/Element")]
+public class ElementObject : ScriptableObject
+{
+    public string elementName;
+}

ffef70bd52a5c73b82b980db5f23f6fa6ab48964
Artur Cruz <arturacruz@icloud.com>
2026-05-06 12:51:51 -0300

    refactored grid code

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public GameObject SelectedCreature;
+    public bool CanBePlaced;
+    public UnityEvent<bool> PlaceCreature = new();
+
+    private void Update()
+    {
+        CanBePlaced = true;
+    }
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this);
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}

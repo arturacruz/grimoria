3926c70bec17906b375c6c758734562a82262ea7
Artur Cruz <arturacruz@icloud.com>
2026-05-13 23:46:45 -0300

    inventory manager

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+    public GridComponent grid;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this);
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}

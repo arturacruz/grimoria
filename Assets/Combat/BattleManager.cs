ed3b8a44afca3857e416ab7ec5f02eb9814dbe13
Artur Cruz <arturacruz@icloud.com>
2026-05-18 22:30:29 -0300

    fix scene name

--- Assets/Combat/BattleManager.cs
+++ Assets/Combat/BattleManager.cs
@@ -70,8 +70,8 @@ public class BattleManager : MonoBehaviour
         enemy.StartBattle();
         tickCooldown.Start();
         
-        //foreach (var creature in enemy.GetGrid())
-        //    creature?.ApplyStatus(Status.StatusEffect.Burn, 40);
+        foreach (var creature in enemy.GetGrid())
+            creature?.ApplyStatus(Status.StatusEffect.Burn, 100);
         
         foreach (var creature in player.GetGrid())
             creature?.DoOnStart(player, enemy);
